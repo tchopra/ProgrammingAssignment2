044cd2349e1d550d33fb3e3cee416fef81d4c55f
tchopra <tc20640n@gmail.com>
2014-05-24 12:02:46 -0400

    Comment changes
    Minor comment changes and spacing to makeCacheMatrix and cacheSolve
    function.

--- cachematrix.R
+++ cachematrix.R
@@ -14,13 +14,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL ## initiate the matrix m to NULL.
+        
         set <- function(y) {
                 x <<- y  ## assigned the pased object (matrix in this case) to the parent variable x.
                 m <<- NULL ## initiate the cached matrix object to null.
         }
-        get <- function() x ## get() function will retun the parent object.
-        setSolve <- function(Solve) m <<- Solve ## setSolve() function will store the inverse of matrix that is passed in the  the cache (m).
+        
+        get <- function() x ## get() function will retun the parent object x
+        
+        setSolve <- function(Solve) m <<- Solve ## setSolve() function will store the inverse of matrix in cache (m).
+        
         getSolve <- function() m ## getSolve() will returns the matrix stored in cache (m).
+        
         list(set = set, get = get,
              setSolve = setSolve,
              getSolve = getSolve) ## listing of all the subfunctions in the makeCacheMatrix() parent function
@@ -36,12 +41,17 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         m <- x$getSolve() ## first check to see if the inverse of matrix is already cached.
+        
         if(!is.null(m)) { ## if the inverese of matrix is cached, then return this matrix back to user
                 message("getting cached data")
                 return(m)
         }
+        
         data <- x$get() ## get the matrix and store it local data variable.
+        
         m <- solve(data,...) ## perform the inverse of matrix. Using the solve() in R in order to compute the inverse of matrix.
+        
         x$setSolve(m) ## store the inverse of passed matrix in the cache
+        
         m ## retun the inverse of matrix.
 }
